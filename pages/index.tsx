ffbf5bedb84697fdd0293af23a0a4036242f7bf4
Meeky Dekowski <meekscreativelab@gmail.com>
2022-08-31 18:14:49 +0700

    prettier installation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         </h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
