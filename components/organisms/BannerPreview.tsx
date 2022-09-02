27a7d0944bc145c6d7e6a8f2cb7be3f4cfee9562
Meeky Dekowski <meekscreativelab@gmail.com>
2022-09-03 00:20:37 +0700

    arrow replace to image instead of svg

--- components/organisms/BannerPreview.tsx
+++ components/organisms/BannerPreview.tsx
@@ -12,7 +12,7 @@ const BannerPreview = () => {
 
   const printRef = useRef<HTMLDivElement>(null);
 
-  const { watch } = useFormContext<BannerFormFields>();
+  const { watch, reset } = useFormContext<BannerFormFields>();
 
   const { banner_background, project_logo, your_name, friendly_introduction, current_mrr, goal_mrr } = watch();
 
@@ -22,12 +22,7 @@ const BannerPreview = () => {
     if (element) {
       setOpen(true);
 
-      const canvas = await html2canvas(element, {
-        scrollX: -window.scrollX,
-        scrollY: -window.scrollY,
-        windowWidth: document.documentElement.offsetWidth,
-        windowHeight: document.documentElement.offsetHeight,
-      });
+      const canvas = await html2canvas(element);
 
       const data = canvas.toDataURL('image/jpg');
 
@@ -43,6 +38,11 @@ const BannerPreview = () => {
       } else {
         window.open(data);
       }
+
+      reset({
+        banner_background: 'bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500',
+        your_name: '',
+      });
     }
   };
 
@@ -101,47 +101,54 @@ const BannerPreview = () => {
         </div>
       </div>
 
-      <div
-        ref={printRef}
-        className={clsx(
-          banner_background,
-          'relative w-full rounded-md shadow h-60 flex flex-col items-center justify-center p-8 text-center'
-        )}
-      >
-        <div className="bg-white/20 flex items-center justify-center text-[0.65rem] absolute top-3 right-3 rounded-full py-0.5 px-2">
-          Banner by openbanner.co
-        </div>
-        <div className="text-[0.65rem] absolute top-3 left-3">
-          Last updated: {new Date().toLocaleString('default', { month: 'long', year: 'numeric' })}
-        </div>
-        <div>
-          {project_logo && (
-            <Image
-              width={100}
-              height={30}
-              className="object-contain -mb-4"
-              src={project_logo.preview}
-              alt="Preview"
-              onLoad={() => URL.revokeObjectURL(project_logo.preview)}
-            />
+      <div className="rounded-md overflow-hidden w-full">
+        <div
+          ref={printRef}
+          className={clsx(
+            banner_background,
+            'relative w-full shadow h-72 flex flex-col items-center justify-center p-8 text-center'
           )}
-          <div className="space-y-2 max-w-sm">
-            <h2 className="text-2xl font-semibold">Hi there{your_name ? `, i'm ${your_name}` : ''}</h2>
-            {friendly_introduction && (
-              <p className="text-md font-normal flex-wrap leading-tight opacity-90 text-sm">{friendly_introduction}</p>
+        >
+          <div className="bg-white/20 flex items-center justify-center text-[0.65rem] absolute top-3 right-3 rounded-full py-0.5 px-2">
+            Banner by openbanner.co
+          </div>
+          <div className="text-[0.65rem] absolute top-3 left-3">
+            Last updated: {new Date().toLocaleString('default', { month: 'long', year: 'numeric' })}
+          </div>
+          <div>
+            {project_logo && (
+              <Image
+                width={100}
+                height={30}
+                className="object-contain -mb-4"
+                src={project_logo.preview}
+                alt="Preview"
+                onLoad={() => URL.revokeObjectURL(project_logo.preview)}
+              />
             )}
-            {current_mrr && (
-              <p className="text-md font-semibold flex-wrap leading-tight text-base">
-                💸 Revenue: ${Intl.NumberFormat('en', { notation: 'compact' }).format(current_mrr)} MRR
-              </p>
-            )}
-            <div className="absolute bottom-16 right-2 text-xs py-1 px-2 w-36">
-              <div>
-                Follow my journey{' '}
-                {goal_mrr ? `to $${Intl.NumberFormat('en', { notation: 'compact' }).format(goal_mrr)} MRR` : ''}
+            <div className="space-y-2 max-w-sm">
+              <h2 className="text-2xl font-semibold">Hi there{your_name ? `, i'm ${your_name}` : ''}</h2>
+              {friendly_introduction && (
+                <p className="text-md font-normal flex-wrap leading-tight opacity-90 text-sm">
+                  {friendly_introduction}
+                </p>
+              )}
+              {current_mrr && (
+                <p className="text-md font-semibold flex-wrap leading-tight text-base">
+                  💸 Revenue: ${Intl.NumberFormat('en', { notation: 'compact' }).format(current_mrr)} MRR
+                </p>
+              )}
+              <div className="absolute bottom-16 right-2 text-xs py-1 px-2 w-36">
+                <div>
+                  Follow my journey{' '}
+                  {goal_mrr ? `to $${Intl.NumberFormat('en', { notation: 'compact' }).format(goal_mrr)} MRR` : ''}
+                </div>
               </div>
-            </div>
-            <svg
+              <span className="absolute bottom-4 right-8">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src="/static/arrow.png" alt="arrow" style={{ width: 29, height: 45 }} />
+              </span>
+              {/* <svg
               width="29"
               height="45"
               viewBox="0 0 29 45"
@@ -155,7 +162,8 @@ const BannerPreview = () => {
                 d="M0.702899 0.959923C0.992298 0.193407 1.84829 -0.193373 2.6148 0.0960263C5.61083 1.22718 11.5005 4.9942 15.4606 12.0937C19.1421 18.6935 21.0939 28.0595 17.6916 40.6447L26.5434 36.1991C27.2756 35.8314 28.1672 36.1269 28.5349 36.859C28.9026 37.5912 28.6072 38.4829 27.875 38.8506L15.9435 44.8429C15.2113 45.2106 14.3197 44.9151 13.952 44.1829L7.95969 32.2514C7.59197 31.5192 7.88743 30.6276 8.6196 30.2599C9.35178 29.8922 10.2434 30.1876 10.6111 30.9198L14.9242 39.5078C18.0075 27.7927 16.1069 19.343 12.8694 13.5391C9.27463 7.09458 3.95541 3.77365 1.5668 2.87183C0.80028 2.58243 0.4135 1.72644 0.702899 0.959923Z"
                 fill="currentColor"
               />
-            </svg>
+            </svg> */}
+            </div>
           </div>
         </div>
       </div>
